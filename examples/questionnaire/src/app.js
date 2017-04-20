02cc1390a49efe06cbb4253fe3f5986089d39576
Mike Bild <mike.bild@googlemail.com>
2017-04-20 08:36:04 +0200

    Layout & Components

--- examples/questionnaire/src/app.js
+++ examples/questionnaire/src/app.js
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom'
 import QuestionnaireButton from './components/QuestionnaireButton'
 import Layout from './pages/Layout'
 
-let uu = false;
-setTimeout(() => {
-  uu = true
-}, 3000)
-
 ReactDOM.render(
   <Layout title={'Questionnaire'} description={'Ipsum Larum Löffelstiel'}>
-    <QuestionnaireButton enabled={uu} />
+    <QuestionnaireButton />
   </Layout>,
   document.getElementById('root')
 )
