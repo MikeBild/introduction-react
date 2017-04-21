fc232b414a3b00697b769f2c7a0e893873ed62cf
Mike Bild <mike.bild@googlemail.com>
2017-04-21 14:46:35 +0200

    Prod

--- examples/questionnaire/src/pages/Layout.js
+++ examples/questionnaire/src/pages/Layout.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import {Questionnaire} from './Questionnaire'
+import {connect} from 'react-redux'
 
 const Layout = ({title = 'Foo', description = 'Bar', children}, context) => {
   return (
     <div>
       <h1>{(context.store.questionnaireData || {}).title || title}</h1>
-      <p>{description}</p>
+      <p>{(context.store.questionnaireData || {}).description || description}</p>
       <div>
         {children}
       </div>
+      <footer>
+        <pre>{process.env.NODE_ENV}:{process.env.VERSION}</pre>
+      </footer>
     </div>
   )
 }
@@ -22,4 +26,11 @@ Layout.propTypes = {
   description: React.PropTypes.string.isRequired,
 }
 
-export default Layout
+function mapStateToProps(state) {
+  return {
+    title: state.title,
+    description: state.description,
+  }
+}
+
+export default connect(mapStateToProps)(Layout)
---
+++ examples/questionnaire/webpack.config.prod.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: ['babel-polyfill', './src/app.js'],
+  output: {
+    path:  __dirname + '/build',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.css$/, loader: 'style-loader!css-loader'},
+      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        VERSION: JSON.stringify(require('./package.json').version),
+        QUESTIONNAIRE_URL: JSON.stringify('http://example.com'),
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+}
