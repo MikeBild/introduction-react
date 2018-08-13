0532d57cfe986b544871f3895274f0454086c40c
Mike Bild <mike.bild@googlemail.com>
2018-08-13 16:00:40 +0200

    webpack build

--- examples/first-babel-webpack-app/src/app.js
+++ examples/first-babel-webpack-app/src/app.js
@@ -1,4 +1,4 @@
 import React from "react";
 import { render } from "react-dom";
 
-render(<h1>Hello React!</h1>, document.getElementById("root"));
+render(<h1>Hello World!</h1>, document.getElementById("root"));
