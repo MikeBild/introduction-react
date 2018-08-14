0ceae9e898678082b7c167ab6383a70b3d7c7ef3
Mike Bild <mike.bild@googlemail.com>
2018-08-14 12:29:02 +0200

    Forms / input elements

--- examples/first-babel-webpack-app/src/components/molecules/PersonalData.js
+++ examples/first-babel-webpack-app/src/components/molecules/PersonalData.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 
 export default class PersonalData extends Component {
   constructor({ activated = true }) {
     super();
+    this.nameInput = createRef();
     this.state = {
       activated
     };
@@ -12,17 +13,33 @@ export default class PersonalData extends Component {
     this.setState({ activated: false });
   }
 
+  onSubmit(evt) {
+    evt.preventDefault();
+    this.deactivateButton();
+
+    const body = {
+      name: this.nameInput.value
+    };
+    console.log(body);
+    //validate -> fetch(...url, {body}) -> error || success -> activateButton
+  }
+
   render() {
     return (
       <div>
-        <span>Name</span>
-        <input type="text" />
-        <button
-          disabled={!this.state.activated}
-          onClick={() => this.deactivateButton()}
-        >
-          Ansenden
-        </button>
+        <form onSubmit={evt => this.onSubmit(evt)}>
+          <input
+            type="text"
+            required
+            ref={elm => (this.nameInput = elm)}
+            onBlur={evt => this.setState({ name: evt.target.value })}
+          />
+          <input
+            type="submit"
+            disabled={!this.state.activated}
+            value="Absenden"
+          />
+        </form>
       </div>
     );
   }
