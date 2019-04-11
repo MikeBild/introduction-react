d38bcf14330a99bf7b2df4e146b6cdea9c1e1b67
Mike Bild <mike.bild@googlemail.com>
2019-04-11 16:13:29 +0200

    add user mocks to view

--- examples/user-management/src/components/pages/Home.jsx
+++ examples/user-management/src/components/pages/Home.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Layout } from '../templates/Layout';
+import users from '../../users.json';
 
 export const Home = () => {
   return (
     <Layout title="Home">
-      <p>Home Content</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Date of Birth</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(({ id, sex, name, birthdate }) => (
+            <tr key={id}>
+              <td>{name}</td>
+              <td>{sex}</td>
+              <td>{birthdate}</td>
+              <td>
+                <>
+                  <Link to={`/user/${id}`}>Change</Link>
+                  <button>Delete</button>
+                </>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Layout>
   );
 };
--- examples/user-management/src/components/pages/User.jsx
+++ examples/user-management/src/components/pages/User.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Layout } from '../templates/Layout';
+import users from '../../users.json';
 
 export const User = ({
   match: {
-    params: { id = 'anonymous' },
+    params: { id: currentUserId = 'anonymous' },
   },
 }) => {
+  const { name = 'anonymous' } = users.find(({ id }) => id === currentUserId);
+
   return (
     <Layout title="User">
-      <p>User Content for ID {id}</p>
+      <p>User Content for ID {currentUserId}</p>
+      <hr />
+      <label>{name}</label>
     </Layout>
   );
 };
