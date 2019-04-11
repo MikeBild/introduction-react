ca7bac2f5b5b5149a11630e001f28db6a44f909d
Mike Bild <mike.bild@googlemail.com>
2019-04-11 15:25:29 +0200

    feat: more pages

--- examples/user-management/src/components/templates/layout.jsx
+++ examples/user-management/src/components/templates/layout.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 export const Layout = ({ title = 'Change please!', children }) => {
@@ -5,6 +6,16 @@ export const Layout = ({ title = 'Change please!', children }) => {
     <>
       <header>
         <h1>{title}</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
       <main>{children}</main>
       <footer />
