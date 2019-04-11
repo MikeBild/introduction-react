0bc86a80d1a3e743e148ee378e3d149794dc9911
Mike Bild <mike.bild@googlemail.com>
2019-04-11 15:27:59 +0200

    feat: about page

---
+++ examples/user-management/src/components/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout } from '../templates/Layout';
+
+export const About = () => {
+  return (
+    <Layout title="About">
+      <p>About Content</p>
+    </Layout>
+  );
+};
+
+export default About;
--- examples/user-management/src/index.jsx
+++ examples/user-management/src/index.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './components/pages/Home';
 import { User } from './components/pages/User';
+import { About } from './components/pages/About';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/user" component={User} />
+        <Route exact path="/about" component={About} />
       </Switch>
     </Router>
   );
