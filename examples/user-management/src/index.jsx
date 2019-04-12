651fbfe703cbed9d7b555e44cdbadbce3e6253f6
Mike Bild <mike.bild@googlemail.com>
2019-04-12 12:19:32 +0200

    use hooks

--- examples/user-management/src/index.jsx
+++ examples/user-management/src/index.jsx
@@ -1,5 +1,5 @@
 import 'babel-polyfill';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './components/pages/Home';
@@ -7,57 +7,48 @@ import { User } from './components/pages/User';
 import { UserAdd } from './components/pages/UserAdd';
 import { About } from './components/pages/About';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = { users: [] };
-  }
+const App = () => {
+  const [users, setUsers] = useState([]);
 
-  async componentDidMount() {
-    try {
-      const users = await delayFetchUsers();
-      this.setState({
-        users,
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  useEffect(() => {
+    (async () => {
+      try {
+        const users = await delayFetchUsers();
+        setUsers(users);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  });
 
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            component={() => <Home users={this.state.users} />}
-          />
-          <Route
-            exact
-            path="/user/add"
-            component={() => (
-              <UserAdd
-                onAdd={newUser =>
-                  this.state.users.push({
-                    ...newUser,
-                    id: Date.now().toString(),
-                  })
-                }
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/user/:id?"
-            component={props => <User {...props} users={this.state.users} />}
-          />
-          <Route exact path="/about" component={About} />
-        </Switch>
-      </Router>
-    );
-  }
-}
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={() => <Home users={users} />} />
+        <Route
+          exact
+          path="/user/add"
+          component={() => (
+            <UserAdd
+              onAdd={newUser =>
+                users.push({
+                  ...newUser,
+                  id: Date.now().toString(),
+                })
+              }
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/user/:id?"
+          component={props => <User {...props} users={users} />}
+        />
+        <Route exact path="/about" component={About} />
+      </Switch>
+    </Router>
+  );
+};
 
 render(<App />, document.getElementById('root'));
 
@@ -74,8 +65,9 @@ async function delayFetchUsers() {
 
 function delay(time) {
   return new Promise(resolve => {
-    setTimeout(() => {
+    const timeoutRef = setTimeout(() => {
       resolve();
+      clearTimeout(timeoutRef);
     }, time);
   });
 }
