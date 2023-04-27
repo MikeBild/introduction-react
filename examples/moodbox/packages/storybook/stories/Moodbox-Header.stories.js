287780538f95fa986fabc40bd887966d097a38f5
Mike Bild <mike@mikebild.com>
2023-04-27 11:26:34 +0200

    feat(storybook): add stories

---
+++ examples/moodbox/packages/storybook/stories/Moodbox-Header.stories.js
@@ -0,0 +1,12 @@
+import { Header } from "@moodbox/ui-components";
+
+export default {
+  title: "Moodbox/Header",
+  component: Header,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export const Default = {};
