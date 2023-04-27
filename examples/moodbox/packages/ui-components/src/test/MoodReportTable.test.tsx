31dd9ed8678fe48e7b3ac6d0eaadc39b4b7d53cb
Roth Hannes (GS/BDA1) <Hannes.Roth2@de.bosch.com>
2023-04-27 15:56:17 +0200

    delete first

---
+++ examples/moodbox/packages/ui-components/src/test/MoodReportTable.test.tsx
@@ -0,0 +1,18 @@
+/* import { render } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { MoodReportTable } from "..";
+import { MoodReportTableAll } from "../organisms/MoodReportTable";
+
+describe("MoodReportTable", () => {
+  it("Bar", () => {
+
+    const props:MoodReportTableAll = {header:{['Day':""]}}
+    render(
+      <MoodReportTable props=props>
+        <h4>Content</h4>
+      </MoodReportTable>
+    );
+    expect(1).toEqual(1);
+  });
+});
+ */
