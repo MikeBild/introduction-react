88ea550ca54b1fde9d8116c39eca84b88a7e959d
Roth Hannes (GS/BDA1) <Hannes.Roth2@de.bosch.com>
2023-04-27 15:56:17 +0200

    delete first

--- examples/moodbox/packages/ui-components/src/organisms/MoodReportTable.tsx
+++ examples/moodbox/packages/ui-components/src/organisms/MoodReportTable.tsx
@@ -1,3 +1,36 @@
-export function MoodReportTable() {
-  return null;
+/*
+
+interface MoodReportTableDataProps {
+  [colName: string]: string | number | Date;
+  //isLoading: boolean;
 }
+
+interface MoodReportTableHeaderProps {
+  [headerName: string]: string;
+}
+
+export interface MoodReportTableAll {
+  rows: MoodReportTableDataProps;
+  header: MoodReportTableHeaderProps;
+}
+
+export function MoodReportTable(props: MoodReportTableAll) {
+  const tableHeader = Object.keys(props.header).map((key) => (
+    <th key={key}>
+      <td>{props.header[key]}</td>
+    </th>
+  ));
+
+  const tableRows = Object.keys(props.rows).map((key) => (
+    <tr key={key}>
+      <td>{props.rows[key].toString()}</td>
+    </tr>
+  ));
+  return (
+    <table>
+      {tableHeader}
+      {tableRows}
+    </table>
+  );
+}
+*/
