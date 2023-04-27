f73f98997e53ab63485d603f413ff667e61c27fd
Samuel Abt <samuel.abt@de.bosch.com>
2023-04-27 15:48:17 +0200

    feat(ui-components/storybook): add InputField

--- examples/moodbox/packages/ui-components/src/molecules/InputField.tsx
+++ examples/moodbox/packages/ui-components/src/molecules/InputField.tsx
@@ -1,3 +1,24 @@
-export function InputField() {
-  return null;
+import { useState } from "react";
+
+interface InputFieldProps {
+  name?: string;
+  label?: string;
+  onValueChange: (arg0: string) => void;
+  initialValue?: string;
+}
+
+export function InputField({ name, label, onValueChange: onValueChange, initialValue }: InputFieldProps) {
+  const [value, setValue] = useState(initialValue || "");
+
+  function onValueChangeInternal(event: any) {
+    setValue(event.target.value)
+    onValueChange && onValueChange(value)
+  }
+  
+  return (
+    <>
+      <label>{label}</label>
+      <input type="text" name={name} onChange={onValueChangeInternal} value={value} />
+    </>
+  );
 }
