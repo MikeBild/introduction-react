8be2e63d6271645603cf9ec07435bd62778a5bdd
Celine Wichmann <Celine.Wichmann@de.bosch.com>
2023-04-28 09:48:07 +0200

    fix missing components

--- examples/moodbox/packages/ui-components/src/organisms/MoodSlider.tsx
+++ examples/moodbox/packages/ui-components/src/organisms/MoodSlider.tsx
@@ -15,16 +15,16 @@ export function MoodSlider(props: MoodSliderProps) {
 
   const StyledSlider = styled.input` 
       width: 100%; 
+      
       -webkit-appearance: none;  /* Override default CSS styles */
-      appearance: none;
       width: 100%; /* Full-width */
-      height: 25px; /* Specified height */
+      height: 15px; /* Specified height */
+      border-radius: 5px;
       background: #d3d3d3; /* Grey background */
       outline: none; /* Remove outline */
       opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
       -webkit-transition: .2s; /* 0.2 seconds transition on hover */
       transition: opacity .2s;
-      border-radius: 50%
 
       &:hover {
         opacity: 1; /* Fully shown on mouse-over */
@@ -54,11 +54,15 @@ export function MoodSlider(props: MoodSliderProps) {
     <h3>Mood: {moodRangePercentage}</h3>
     <div>
         <StyledSlider type="range" min={1} max={100} value={moodRangePercentage} step={1} list="moodSection" onChange={moodChange}></StyledSlider>
-
-        <datalist id="moodSection">
-          <option value="50" label="Bad"></option>
-        </datalist>
     </div>
+
+    <datalist id="moodSection">
+          <option value="0" label="Worst day of my life"></option>
+          <option value="30" label="Bad"></option>
+          <option value="50" label="Neutral"></option>
+          <option value="70" label="Good"></option>
+          <option value="100" label="Best day of my life"></option>
+        </datalist>
 
     <button onClick={() => props.confirmMood(moodRangePercentage)}>Confirm</button>
     </>
