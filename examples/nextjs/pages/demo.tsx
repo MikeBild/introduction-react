d74b400d076d1bcc544c55f615a6e326ae806c3c
Mike Bild <mikebild@M3Pro14.fritz.box>
2024-06-01 19:50:56 +0200

    add NextJS

---
+++ examples/nextjs/pages/demo.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout";
+
+export default function Demo() {
+  return (
+    <Layout>
+      <h1>Demo Mike!</h1>
+      <a href="/">Index Link</a>
+    </Layout>
+  );
+}
---
+++ examples/nextjs/pages/index.tsx
@@ -0,0 +1,10 @@
+import RootLayout from "../components/layout";
+
+export default function Index() {
+  return (
+    <RootLayout>
+      <h1>Hello, Next.js!</h1>
+      <a href="/demo">Demo Link</a>
+    </RootLayout>
+  );
+}
