8580f3c1c8b63b9a293a73b3ab72b7f99eef65af
Mike Bild <mike.bild@gmail.com>
2024-06-05 15:00:33 +0200

    more

--- examples/nextjs-todo-app/client/pages/login.tsx
+++ examples/nextjs-todo-app/client/pages/login.tsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
 import { useUserContext } from "../lib/userProvider";
 import { Layout } from "../components/templates/Layout";
+import { useRouter } from "next/router";
 
 export default function Login() {
+  const router = useRouter();
   const [usernameInput, setUsernameInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
   const [userContext, setUser] = useUserContext();
+
+  async function login(username: string, password: string) {
+    const response = await fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    return await response.json();
+  }
 
   return (
     <Layout>
@@ -19,8 +33,10 @@ export default function Login() {
         onChange={(evt) => setPasswordInput(evt.target.value)}
       />
       <button
-        onClick={() => {
-          setUser({ username: usernameInput });
+        onClick={async () => {
+          const result = await login(usernameInput, passwordInput);
+          setUser(result);
+          router.push("/");
         }}
       >
         Login
