aec679e1ddb8924d0c1263a6af6363fc529f365b
Mike Bild <mike.bild@gmail.com>
2024-06-05 15:50:45 +0200

    styles

--- examples/nextjs-todo-app/client/pages/about.tsx
+++ examples/nextjs-todo-app/client/pages/about.tsx
@@ -1,19 +1,17 @@
-import { Header } from "../components/organisms/Header";
-import { MenuList } from "../components/organisms/MenuList";
 import { Layout } from "../components/templates/Layout";
 import { useDelay } from "../lib/useDelay";
 import { useFetch } from "../lib/useFetch";
+import styles from "../styles/about.module.css";
 
 export default function About() {
   const data = useDelay(5000);
   const tData = useFetch();
 
-
   return (
     <Layout>
       <h1>About</h1>
       <hr />
-      <div>{JSON.stringify(data, null, 4)}</div>
+      <div className={styles.foo}>{JSON.stringify(data, null, 4)}</div>
       <div>{JSON.stringify(tData, null, 4)}</div>
     </Layout>
   );
