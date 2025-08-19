8dee782a11eb7f4f75eead2a75730614ac677586
Mike Bild <mike.bild@gmail.com>
2025-08-19 14:55:58 +0200

    build the app

--- examples/endgame/src/App.tsx
+++ examples/endgame/src/App.tsx
@@ -1,26 +1,28 @@
-import "./App.css";
 import { useState } from "react";
-import SignupScreen from "./components/SignupScreen";
+import "./App.css";
+import Layout from "./components/Layout";
+import { useAuthContext } from "./components/AuthProvider";
+import LoginSignupScreen from "./components/LoginScreen";
 
 export default function App() {
-const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const auth = useAuthContext();
+  
+  return (
+    <Layout
+      loggedInUser={auth?.isAuthenticated ? username : null}
+      setLoggedInUser={() => setUsername("")}
+    >
+      {auth?.isAuthenticated && <div>logged in</div>}
 
-  return <>
-  {/* if(!isLoggedIn) { */}
-  <SignupScreen onRegisterSubmit={register}></SignupScreen>
-  {/* } */}
-  </>;
-}
-
-const register = () => {
-  console.log("register");
-  alert(<section>
-    <h1>Welcome to Endgame!</h1>
-    <p>Thou have been authenticated.</p>
-    <p>Thou may now procced.</p>
-</section>)
-}
-
-const login = () => {
-  console.log("login");
+      {!auth?.isAuthenticated && (
+        <LoginSignupScreen
+          onLoginSubmit={(username) => {
+            auth?.login(username);
+            setUsername(username);
+          }}
+        />
+      )}
+    </Layout>
+  );
 }
