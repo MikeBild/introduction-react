544f3e0ba91a51b633d311fc32f3d76e79e5f222
Mike Bild <mike.bild@gmail.com>
2025-08-19 14:11:38 +0200

    add layout log in/out example

--- examples/endgame/src/components/Layout.stories.tsx
+++ examples/endgame/src/components/Layout.stories.tsx
@@ -1,6 +1,7 @@
-import type { Meta, StoryObj } from '@storybook/react-vite';
+import type { Meta, StoryObj } from "@storybook/react-vite";
 
-import Layout from './Layout';
+import Layout from "./Layout";
+import { useState } from "react";
 
 const meta = {
   component: Layout,
@@ -10,10 +11,29 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const LoggedIn: Story = {
+  render: (args: any) => {
+    const [username, setUsername] = useState(args.loggedInUser);
+    return (
+      <Layout loggedInUser={username} setLoggedInUser={() => setUsername("")}>
+        {username && <div>logged in</div>}
+
+        {!username && (
+          <button onClick={() => setUsername(args.loggedInUser)}>Login</button>
+        )}
+      </Layout>
+    );
+  },
+  args: {
+    loggedInUser: "Max Mustermann",
+    setLoggedInUser(user) {},
+  },
+};
+
+export const LoggedOut: Story = {
   render: (args) => <Layout {...args} />,
   args: {
-    loggedInUser: 'Max Mustermann',
+    loggedInUser: null,
     setLoggedInUser: (user) => alert(user),
   },
-}
+};
