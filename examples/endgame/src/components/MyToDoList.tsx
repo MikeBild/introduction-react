dce659a37a8811ff5007b0473e6bb830efb1423d
Mike Bild <mike.bild@gmail.com>
2025-08-19 14:25:03 +0200

    add key to todolist

--- examples/endgame/src/components/MyToDoList.tsx
+++ examples/endgame/src/components/MyToDoList.tsx
@@ -3,21 +3,33 @@ import type { Todo, TodoList } from "./TodoList";
 import "./MyToDoList.css";
 
 interface ToDoListProps {
-    todoList?: TodoList;
-    onItemRemoved: (item: Todo) => void;
-    onItemDoneToggle: (item: Todo) => void;
+  todoList?: TodoList;
+  onItemRemoved: (item: Todo) => void;
+  onItemDoneToggle: (item: Todo) => void;
 }
 
-export default function ToDoList({todoList = {todos: []}, onItemRemoved, onItemDoneToggle: onItemCheckedToggle}: ToDoListProps){
-    if (todoList.todos.length == 0){
-        return <p>Hurray! There are no more to do's.</p>
-    }
-    return (
+export default function ToDoList({
+  todoList = { todos: [] },
+  onItemRemoved,
+  onItemDoneToggle: onItemCheckedToggle,
+}: ToDoListProps) {
+  if (todoList.todos.length == 0) {
+    return <p>Hurray! There are no more to do's.</p>;
+  }
+  return (
     <ul>
-        {todoList.todos.map( (item) => <ToDoListItem 
-        onRemove= { (item) => { onItemRemoved(item)} } 
-        onCheckedToggle= { (item) => { onItemCheckedToggle(item)} } 
-        item={item}></ToDoListItem> )}
-    </ul>)   
+      {todoList.todos.map((item, i) => (
+        <ToDoListItem
+          key={i.toString()}
+          onRemove={(item) => {
+            onItemRemoved(item);
+          }}
+          onCheckedToggle={(item) => {
+            onItemCheckedToggle(item);
+          }}
+          item={item}
+        ></ToDoListItem>
+      ))}
+    </ul>
+  );
 }
-
--- examples/endgame/src/components/ToDoListItem.tsx
+++ examples/endgame/src/components/ToDoListItem.tsx
@@ -1,18 +1,27 @@
 import type { Todo } from "./TodoList";
 
-
-interface ToDoListItemProps{
-    item: Todo;
-    onRemove: (item: Todo) => void;
-    onCheckedToggle: (todoList: Todo) => void;
+interface ToDoListItemProps {
+  item: Todo;
+  key: string;
+  onRemove: (item: Todo) => void;
+  onCheckedToggle: (todoList: Todo) => void;
 }
 
-export default function ToDoListItem({item, onRemove, onCheckedToggle: onDoneToggle}: ToDoListItemProps){
-    return (
-        <li>
-            <input type="checkbox" checked={item.done} onChange={ () => onDoneToggle(item)} />
-            {item.text}
-            <button onClick={ () => onRemove(item) }>Remove</button>
-        </li>
-    )
+export default function ToDoListItem({
+  item,
+  key,
+  onRemove,
+  onCheckedToggle: onDoneToggle,
+}: ToDoListItemProps) {
+  return (
+    <li key={key}>
+      <input
+        type="checkbox"
+        checked={item.done}
+        onChange={() => onDoneToggle(item)}
+      />
+      {item.text}
+      <button onClick={() => onRemove(item)}>Remove</button>
+    </li>
+  );
 }
