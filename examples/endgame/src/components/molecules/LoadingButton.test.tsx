575ea87803c0d02cd05c6fb029a681e9a1789cd4
Mike Bild <mike.bild@gmail.com>
2025-08-20 11:05:46 +0200

    add describe to test

--- examples/endgame/src/components/molecules/LoadingButton.test.tsx
+++ examples/endgame/src/components/molecules/LoadingButton.test.tsx
@@ -4,49 +4,49 @@ import LoadingButton from "./LoadingButton";
 import userEvent from "@testing-library/user-event";
 
 describe("LoadingButton", () => {
-  afterEach(() => {
-    cleanup();
-  });
+  describe("Story default", () => {
+    afterEach(() => {
+      cleanup();
+    });
 
-  it("should render a button with `Loading` content", () => {
-    render(<LoadingButton>Loading</LoadingButton>);
-    expect(screen.getByTestId("loading-button")).toHaveTextContent("Loading");
-  });
+    it("should render a button with `Loading` content", () => {
+      render(<LoadingButton>Loading</LoadingButton>);
+      expect(screen.getByTestId("loading-button")).toHaveTextContent("Loading");
+    });
 
-  it("displays  Text der Kinder an, if not loading", () => {
-    render(<LoadingButton>Send</LoadingButton>);
-    expect(screen.getByTestId("loading-button")).toHaveTextContent("Send");
-    expect(screen.getByTestId("loading-button")).not.toBeDisabled();
-  });
+    it("displays Text, if not loading", () => {
+      render(<LoadingButton>Send</LoadingButton>);
+      expect(screen.getByTestId("loading-button")).toHaveTextContent("Send");
+      expect(screen.getByTestId("loading-button")).not.toBeDisabled();
+    });
 
-  it("displays 'Loading...' and is disabled, if loading=true", () => {
-    render(<LoadingButton loading>Send</LoadingButton>);
-    expect(screen.getByTestId("loading-button")).toHaveTextContent(
-      "loading"
-    );
-    expect(screen.getByTestId("loading-button")).toBeDisabled();
-  });
+    it("displays 'Loading...' and is disabled, if loading=true", () => {
+      render(<LoadingButton loading>Send</LoadingButton>);
+      expect(screen.getByTestId("loading-button")).toHaveTextContent("loading");
+      expect(screen.getByTestId("loading-button")).toBeDisabled();
+    });
 
-  it("invokes onClick, if not loading", async () => {
-    const user = userEvent.setup();
-    const handleClick = vi.fn();
-    render(<LoadingButton onBtnClick={handleClick}>Send</LoadingButton>);
+    it("invokes onClick, if not loading", async () => {
+      const user = userEvent.setup();
+      const handleClick = vi.fn();
+      render(<LoadingButton onBtnClick={handleClick}>Send</LoadingButton>);
 
-    await user.click(screen.getByTestId("loading-button"));
+      await user.click(screen.getByTestId("loading-button"));
 
-    expect(handleClick).toHaveBeenCalledTimes(1);
-  });
+      expect(handleClick).toHaveBeenCalledTimes(1);
+    });
 
-  it("NOT invokes onClick, if loading=true", async () => {
-    const user = userEvent.setup();
-    const handleClick = vi.fn();
-    render(
-      <LoadingButton loading onBtnClick={handleClick}>
-        Send
-      </LoadingButton>
-    );
+    it("NOT invokes onClick, if loading=true", async () => {
+      const user = userEvent.setup();
+      const handleClick = vi.fn();
+      render(
+        <LoadingButton loading onBtnClick={handleClick}>
+          Send
+        </LoadingButton>
+      );
 
-    await user.click(screen.getByTestId("loading-button"));
-    expect(handleClick).not.toHaveBeenCalled();
+      await user.click(screen.getByTestId("loading-button"));
+      expect(handleClick).not.toHaveBeenCalled();
+    });
   });
 });
